f456c87b982ee9955f40a78fbd2b838c99c1a062
visarzejna <visarzejna@gmail.com>
2023-04-13 01:07:27 +0200

    Added useCountries hook that gets all countries from a package, and countrySelect input for the modal

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,8 +1,47 @@
 'use client';
 
-const CountrySelect = () => {
+import useCountries from '@/app/hooks/useCountries';
+import React from 'react';
+import Select from 'react-select'
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: number[];
+  region: string;
+  value: string;
+}
+
+interface CountrySelectProps {
+  value?: CountrySelectValue;
+  onChange: (value: CountrySelectValue) => void;
+}
+
+const CountrySelect: React.FC<CountrySelectProps> = ({
+  value,
+  onChange
+}) => {
+  const { getAll } = useCountries();
+
   return (
-    <div>CountrySelect</div>
+    <div className="
+        
+    ">
+      <Select 
+        placeholder="Anywhere"
+        isClearable
+        options={getAll()}
+        value={value}
+        onChange={(value) => onChange(value as CountrySelectValue)}
+        formatOptionLabel={(option: any) => (
+          <div className='flex flex-row items-center gap-3'>
+            <div>{option.flag}</div>
+           <div>{option.label},
+              <span className='text-neutral-800 ml-1'>{option.region}</span>
+            </div> 
+          </div>
+        )}
+      />
+    </div>
   )
 }
 
